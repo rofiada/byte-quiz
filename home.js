3277295c9899c18e1d9b6470fa1287f2603b1c9a
rofiada <121477846+rofiada@users.noreply.github.com>
2024-02-19 16:47:36 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+
+
+    function q_right() {
+      
+      document.getElementById("answerButton").style.backgroundColor = "green";
+  }
+  function q_wrong() {
+      event.target.style.backgroundColor = "red";
+  }
